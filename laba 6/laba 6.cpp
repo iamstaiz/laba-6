9491476223d88122623418e876d6249bf162ac5c
Nastia <kasianenko.anastasiia@chnu.edu.ua>
2024-05-08 20:23:09 +0300

    add menu

--- laba 6/laba 6.cpp
+++ laba 6/laba 6.cpp
@@ -3,13 +3,33 @@
 using namespace std;
 
 int main(){
-	int punct;
+	int punct = 0;
 	bool exit;
 	exit = 0;
 	while (exit ==0){
-		cout << "1.user";
-		cout << "2.admin";
-		cout << "test";
+		cout << "1.user" << endl;
+		cout << "2.admin"<< endl;
+		cout << "3.exit" << endl;
+		try {
+			string check;
+			cout << "Choose:";
+			cin >> check;
+			size_t size = check.find_first_not_of( "0123456789");
+			if (size == 0)
+				throw string{ "Invalid input!\n" };
+			punct = stoi(check, 0);
+		}
+		catch (string& ex) {
+			cout << ex;
+		}
+		if (punct == 1) {
 
+		}else if(punct == 2){
+
+		}
+		else if (punct == 3) {
+			exit = 1;
+		}
 	}
+	return 0;
 }

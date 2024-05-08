67cb4cd3bec26bee4c59f0a8a2f5534d7d2f0095
Nastia <kasianenko.anastasiia@chnu.edu.ua>
2024-05-08 21:14:45 +0300

    add write file function

--- laba 6/Movie.cpp
+++ laba 6/Movie.cpp
@@ -3,5 +3,15 @@ using namespace std;
 Movie::Movie(string name_, string genre_, int age_) : name{ name_ }, genre{ genre_ }, age{ age_ } {}
 Movie::~Movie(){}
 void Movie::write_file() {
-	ofstream list()
+	ofstream list("Movies.txt", ios::app);
+	if (list.is_open()) {
+		string buff;
+		buff = to_string(this->age);
+		list << buff.length() << " " << this->age << " ";
+		list << this->name.length() << " " << this->name << " ";
+		list << this->genre.length() << " " << this->genre << "\n";
+	}
+	else {
+		cout << " File isn`t open.\n";
+	}
 }
